5c09c6f698a99a1c44adad1dba4f0d31b7f90554
EricOT98 <EricOToole@Outlook.com>
2019-01-22 16:46:04 +0000

    Added main menu, leaderboard menu, multiple component fixes

--- src/Game.js
+++ src/Game.js
@@ -9,6 +9,8 @@ class Game {
    * @desc simple game constructor
    */
   constructor() {
+    this.menuHandler = new MenuHandler();
+    this.initMenus();
   }
 
   init() {
@@ -31,5 +33,69 @@ class Game {
     var ctx = canv.getContext("2d");
     ctx.clearRect(0, 0, canv.width, canv.height);
     console.log("Draw");
+  }
+
+  initMenus() {
+    let canvas = document.getElementById("canvas");
+    let mainDiv = document.createElement('div');
+    mainDiv.style.position = "relative";
+    mainDiv.style.width = canvas.width + "px";
+    mainDiv.style.height = canvas.height + "px";
+    mainDiv.appendChild(canvas);
+    document.body.appendChild(mainDiv);
+
+    let mainMenuScene = new Scene("Main Menu", mainDiv,
+        {'x': 0,'y': 0, 'width': 100, 'height': 100}
+        );
+    let mainMenu = new Menu("Main Menu",
+        {'x': 20, 'y': 20, 'width': 60, 'height': 60}
+        );
+    let playBtn = new Button("Play",
+        this.menuHandler.goToScene.bind(this.menuHandler, "Game"),
+        {'x': 35, 'y': 50, 'width': 30, 'height': 10},
+        "%"
+    );
+
+    playBtn.makeImageButton("assets/ui/play_btn.png");
+    playBtn.addHoverImage("assets/ui/play_btn_pressed.png");
+    mainMenu.addButton("Play", playBtn);
+
+    let leaderboardBtn = new Button("Leaderboard",
+        this.menuHandler.goToScene.bind(this.menuHandler, "Leaderboard"),
+        {'x': 35, 'y': 70, 'width': 30, 'height': 10},
+        "%"
+    );
+
+    leaderboardBtn.makeImageButton("assets/ui/leaderboard_btn.png");
+    leaderboardBtn.addHoverImage("assets/ui/leaderboard_btn_prssed.png");
+    mainMenu.addButton("Leaderboard", leaderboardBtn);
+
+    mainMenuScene.addMenu(mainMenu);
+    mainMenuScene._containerDiv.style.backgroundImage = "url('assets/tempBackground.jpg')";
+    this.menuHandler.addScene("Main Menu", mainMenuScene);
+
+
+    let leaderboardScene = new Scene("Leaderboard", mainDiv,
+        {'x': 0,'y': 0, 'width': 100, 'height': 100}
+    );
+    let leaderboardMenu = new Menu("Leaderboard",
+        {'x': 20, 'y': 20, 'width': 60, 'height': 60}
+    );
+    let backBtn = new Button("Back",
+        this.menuHandler.goToScene.bind(this.menuHandler, "Main Menu"),
+        {'x': 35, 'y': 80, 'width': 30, 'height': 10},
+        "%"
+    );
+    backBtn.makeImageButton("assets/ui/back_btn.png");
+    backBtn.addHoverImage("assets/ui/back_btn_pressed.png");
+    leaderboardMenu.addButton("Back", backBtn);
+    leaderboardScene.addMenu(leaderboardMenu);
+    this.menuHandler.addScene("Leaderboard",leaderboardScene);
+
+    this.menuHandler._theme.setPrimary(0,0,0,1);
+    this.menuHandler._theme.setSecondary(49, 49, 49, 0.5);
+    this.menuHandler._theme.setTertiary(255,190,61, 0);
+    this.menuHandler.applyTheme();
+    this.menuHandler.showOnlyCurrentScene();
   }
 }
