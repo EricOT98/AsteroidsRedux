a1a443c2a31de065cd284dc023aaeea458137f03
MichaelBridgette <c00205948@itcarlow.ie>
2019-01-22 14:42:40 +0000

    Bullets can be fired

--- src/Projectile.js
+++ src/Projectile.js
@@ -1,13 +1,31 @@
 class Projectile{
 
-  constructor(x,y){
+  constructor(x,y, angle){
+    this.positionX = x;
+    this.positionY = y;
 
+    this.velocityX = 0;
+    this.velocityY = 0;
+
+    this.angle = angle;
   }
   update(){
+    var radians = this.angle;
+
+    this.velocityX = Math.cos(radians)* 15;
+    this.velocityY = Math.sin(radians)* 15;
+
+    this.positionX -= this.velocityX;
+    this.positionY -= this.velocityY;
 
   }
   draw(ctx){
+    ctx.beginPath();
+    ctx.arc(this.positionX, this.positionY, 5, 0, 2 * Math.PI);
+    ctx.stroke();
 
+
+    
   }
 
 }
