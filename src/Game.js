57f8c75fa5bc6bb4ecff24f00e4d38df0009434f
EricOT98 <EricOToole@Outlook.com>
2019-01-23 18:47:28 +0000

    Switched to class based scene structure

--- src/Game.js
+++ src/Game.js
@@ -159,85 +159,18 @@ class Game {
     mainDiv.style.position = "relative";
     mainDiv.style.width = canvas.width + "px";
     mainDiv.style.height = canvas.height + "px";
+    mainDiv.id = "main-div";
     mainDiv.appendChild(canvas);
     document.body.appendChild(mainDiv);
 
-    let mainMenuScene = new Scene("Main Menu", mainDiv,
-        {'x': 0,'y': 0, 'width': 100, 'height': 100}
-    );
-    let mainMenu = new Menu("Main Menu",
-        {'x': 20, 'y': 20, 'width': 60, 'height': 60}
-    );
-    let playBtn = new Button("Play",
-        this.menuHandler.goToScene.bind(this.menuHandler, "Game"),
-        {'x': 35, 'y': 50, 'width': 30, 'height': 10},
-        "%"
-    );
-
-    playBtn.makeImageButton("assets/ui/play_btn.png");
-    playBtn.addHoverImage("assets/ui/play_btn_pressed.png");
-    mainMenu.addButton("Play", playBtn);
-
-    let leaderboardBtn = new Button("Leaderboard",
-        this.menuHandler.goToScene.bind(this.menuHandler, "Leaderboard"),
-        {'x': 35, 'y': 65, 'width': 30, 'height': 10},
-        "%"
-    );
-
-    leaderboardBtn.makeImageButton("assets/ui/leaderboard_btn.png");
-    leaderboardBtn.addHoverImage("assets/ui/leaderboard_btn_prssed.png");
-    mainMenu.addButton("Leaderboard", leaderboardBtn);
-
-    let controlsBtn = new Button("Controls",
-        this.menuHandler.goToScene.bind(this.menuHandler, "Controls"),
-        {'x': 35, 'y': 80, 'width': 30, 'height': 10},
-        "%"
-    );
-
-    controlsBtn.makeImageButton("assets/ui/control_btn.png");
-    controlsBtn.addHoverImage("assets/ui/control_btn_pressed.png");
-    mainMenu.addButton("Controls", controlsBtn);
-
-    mainMenuScene.addMenu(mainMenu);
-    mainMenuScene._containerDiv.style.backgroundImage = "url('assets/tempBackground.jpg')";
+    let mainMenuScene = new MainMenuScene(this.menuHandler);
     this.menuHandler.addScene("Main Menu", mainMenuScene);
 
-
-    let leaderboardScene = new Scene("Leaderboard", mainDiv,
-        {'x': 0,'y': 0, 'width': 100, 'height': 100}
-    );
-    let leaderboardMenu = new Menu("Leaderboard",
-        {'x': 20, 'y': 20, 'width': 60, 'height': 60}
-    );
-    let backBtn = new Button("Back",
-        this.menuHandler.goToScene.bind(this.menuHandler, "Main Menu"),
-        {'x': 35, 'y': 80, 'width': 30, 'height': 10},
-        "%"
-    );
-    backBtn.makeImageButton("assets/ui/back_btn.png");
-    backBtn.addHoverImage("assets/ui/back_btn_pressed.png");
-    leaderboardMenu.addButton("Back", backBtn);
-    leaderboardScene.addMenu(leaderboardMenu);
+    let leaderboardScene = new LeaderboardScene(this.menuHandler);
     this.menuHandler.addScene("Leaderboard",leaderboardScene);
 
-    let controlScene = new Scene("Controls", mainDiv,
-        {'x': 0,'y': 0, 'width': 100, 'height': 100}
-    );
-
-    let controlsMenu = new Menu("Leaderboard",
-        {'x': 20, 'y': 20, 'width': 60, 'height': 60}
-    );
-    let backBtn2 = new Button("Back",
-        this.menuHandler.goToScene.bind(this.menuHandler, "Main Menu"),
-        {'x': 35, 'y': 80, 'width': 30, 'height': 10},
-        "%"
-    );
-    backBtn2.makeImageButton("assets/ui/back_btn.png");
-    backBtn2.addHoverImage("assets/ui/back_btn_pressed.png");
-    controlsMenu.addButton("Back", backBtn);
-    controlScene.addMenu(controlsMenu);
+    let controlScene = new ControlsScene(this.menuHandler);
     this.menuHandler.addScene("Controls", controlScene);
-
     let gameScene = new Scene("Game", mainDiv,
         {'x': 0,'y': 0, 'width': 100, 'height': 100}
     );
